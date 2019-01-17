fa294b3d7fc0e027502516890a653d8f319a1392
1002542303@qq.com <7893507meng>
2019-01-17 15:58:14 +0800

    init repository

---
+++ src/main/java/meng/learn/security/SecurityApplication.java
@@ -0,0 +1,14 @@
+package meng.learn.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+
+}
+
